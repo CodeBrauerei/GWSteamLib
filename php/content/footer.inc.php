1f162a1cb161bc24f4a49c20d41e3cc1f476e751
Gabriel Wanzek <gabrielwanzek@ymail.com>
2013-10-12 23:45:40 +0200

    Update footer.inc.php

--- php/content/footer.inc.php
+++ php/content/footer.inc.php
@@ -1,3 +1,3 @@
         <div class="container" style="text-align: center;padding-top: 15px;padding-bottom: 5px;">
-            <a href="https://github.com/GabrielWanzek/GWSteamLib" class="btn btn-info btn-xs">GWSteamLib v0.2</a>
+            <a href="https://github.com/GabrielWanzek/GWSteamLib" class="btn btn-info btn-xs">GWSteamLib v0.3</a>
         </div>
